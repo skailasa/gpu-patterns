9128fa71aea759f46761d7d9b1daa569512e0432
Srinath Kailasa <srinathkailasa@gmail.com>
2025-09-13 15:27:18 +0000

    Add two dim prefix sum

--- patterns/cpu_patterns/__init__.py
+++ patterns/cpu_patterns/__init__.py
@@ -1,2 +1,17 @@
-from . import kernels
+import os
+import pathlib
 
+from torch.utils.cpp_extension import load
+
+root = pathlib.Path(__file__).parent
+kernels = root / "kernels"
+sources = [*(kernels.glob("*.cpp"))]
+sources = [str(p) for p in sources]
+
+kernels = load(
+    name="cpu_kernels",
+    sources=sources,
+    extra_cflags=["-O3", "-fopenmp"],
+    extra_ldflags=["-fopenmp"],
+    verbose=True,
+)
